6c7cea19f2dfc296de361fec8dddc764cd3b4cf0
Ed Angelis <ed4ngelis@gmail.com>
2025-11-08 11:23:20 -0300

    audios

---
+++ constants/audios-references/esta-e-a-letra.constant.ts
@@ -0,0 +1,111 @@
+export type LetterAnswerAudio = {
+  path: string;
+  reference: string;
+};
+
+export const ESTA_E_A_LETRA: LetterAnswerAudio[] = [
+  {
+    reference: "a",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-a.mp3",
+  },
+  {
+    reference: "b",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-b.mp3",
+  },
+  {
+    reference: "c",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-c.mp3",
+  },
+  {
+    reference: "d",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-d.mp3",
+  },
+  {
+    reference: "e",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-e.mp3",
+  },
+  {
+    reference: "f",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-f.mp3",
+  },
+  {
+    reference: "g",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-g.mp3",
+  },
+  {
+    reference: "h",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-h.mp3",
+  },
+  {
+    reference: "i",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-i.mp3",
+  },
+  {
+    reference: "j",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-j.mp3",
+  },
+  {
+    reference: "k",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-k.mp3",
+  },
+  {
+    reference: "l",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-l.mp3",
+  },
+  {
+    reference: "m",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-m.mp3",
+  },
+  {
+    reference: "n",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-n.mp3",
+  },
+  {
+    reference: "o",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-o.mp3",
+  },
+  {
+    reference: "p",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-p.mp3",
+  },
+  {
+    reference: "q",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-q.mp3",
+  },
+  {
+    reference: "r",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-r.mp3",
+  },
+  {
+    reference: "s",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-s.mp3",
+  },
+  {
+    reference: "t",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-t.mp3",
+  },
+  {
+    reference: "u",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-u.mp3",
+  },
+  {
+    reference: "v",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-v.mp3",
+  },
+  {
+    reference: "w",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-w.mp3",
+  },
+  {
+    reference: "x",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-x.mp3",
+  },
+  {
+    reference: "y",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-y.mp3",
+  },
+  {
+    reference: "z",
+    path: "/assets/audio/esta-e-a-letra/esta-e-a-letra-z.mp3",
+  },
+];
---
+++ constants/audios-references/este-e-o-numero.constant.ts
@@ -0,0 +1,51 @@
+export type NumberAnswerAudio = {
+  path: string;
+  reference: string;
+};
+
+export const QUAL_O_NUMERO: NumberAnswerAudio[] = [
+  {
+    reference: "0",
+    path: "/assets/audio/este-e-o-numero/este-e-o-numero-0.mp3",
+  },
+  {
+    reference: "1",
+    path: "/assets/audio/este-e-o-numero/este-e-o-numero-1.mp3",
+  },
+  {
+    reference: "2",
+    path: "/assets/audio/este-e-o-numero/este-e-o-numero-2.mp3",
+  },
+  {
+    reference: "3",
+    path: "/assets/audio/este-e-o-numero/este-e-o-numero-3.mp3",
+  },
+  {
+    reference: "4",
+    path: "/assets/audio/este-e-o-numero/este-e-o-numero-4.mp3",
+  },
+  {
+    reference: "5",
+    path: "/assets/audio/este-e-o-numero/este-e-o-numero-5.mp3",
+  },
+  {
+    reference: "6",
+    path: "/assets/audio/este-e-o-numero/este-e-o-numero-6.mp3",
+  },
+  {
+    reference: "7",
+    path: "/assets/audio/este-e-o-numero/este-e-o-numero-7.mp3",
+  },
+  {
+    reference: "8",
+    path: "/assets/audio/este-e-o-numero/este-e-o-numero-8.mp3",
+  },
+  {
+    reference: "9",
+    path: "/assets/audio/este-e-o-numero/este-e-o-numero-9.mp3",
+  },
+  {
+    reference: "10",
+    path: "/assets/audio/este-e-o-numero/este-e-o-numero-10.mp3",
+  },
+];
